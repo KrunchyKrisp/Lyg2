8d8409b1d359d70f37d9d3004129ef3abb96334f
KrunchyKrisp <m.damidavicius@gmail.com>
2023-03-12 23:37:58 +0200

    Comments

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,84 @@
 /*
+ * Main.java
+ * Matas Damidavičius INF 3k2g1p 1910621
+ * ND2 variantas 1 - Semafora
  *
+ * N gijų naudos bendrą MySemaphore objektą užtikrinti sinchroniškumą keičiant bendrą sharedResource skaitiklio
+ * reikšmę ++/--;
  */
 public class Main {
-    public static int sharedResource = 0;
+	public static int sharedResource = 0; //bendras skaitiklis
 
-    public static void main(String[] args) throws InterruptedException {
-        MySemaphore ms = new MySemaphore(1);
+	public static void main(String[] args) throws InterruptedException {
+		MySemaphore ms = new MySemaphore(1); //semafora su skaitikliu N = 1
 
-        int count = 10;
-        MyThread[] threads = new MyThread[count];
+		int count = 10;
+		MyThread[] threads = new MyThread[count]; //N gijų
 
-        for (int i = 0; i < count; ++i) {
-            threads[i] = new MyThread(ms, i % 2 == 0 ? 1 : -1);
-            threads[i].start();
-        }
+		for (int i = 0; i < count; ++i) {
+			threads[i] = new MyThread(ms, i % 2 == 0 ? 1 : -1); //gija sukuriama su bendra semafora ir 1/-1
+			threads[i].start();
+		}
 
-        for (int i = 0; i < count; ++i) {
-            threads[i].join();
-        }
+		for (int i = 0; i < count; ++i) {
+			threads[i].join();
+		}
 
-        System.out.println("All Threads Done");
-        System.out.println("Semaphore Counter = " + ms.numberAvailable());
-        System.out.println("Shared Resource = " + sharedResource);
-    }
+		System.out.println("All Threads Done");
+		System.out.println("Semaphore Counter = " + ms.numberAvailable());
+		System.out.println("Shared Resource = " + sharedResource);
+	}
 }
 
 class MyThread extends Thread {
-    int changeAmount;
-    MySemaphore ms;
+	int changeAmount; //+1/-1
+	MySemaphore ms; //bendra semafora
 
-    public MyThread(MySemaphore ms, int changeAmount) {
-        this.ms = ms;
-        this.changeAmount = changeAmount;
-    }
+	public MyThread(MySemaphore ms, int changeAmount) {
+		this.ms = ms;
+		this.changeAmount = changeAmount;
+	}
 
-    public void run() {
-        for (int i = 0; i < 1_000_000; ++i) {
-            try {
-                ms.request();
+	public void run() { //1_000_000 kartų, pasinaudojant semafora, ++/-- bendrą skaitiklį
+		for (int i = 0; i < 1_000_000; ++i) {
+			try {
+				ms.request();
 
-                int temp = Main.sharedResource;
-                temp += changeAmount;
-                Main.sharedResource = temp;
+				int temp = Main.sharedResource;
+				temp += changeAmount;
+				Main.sharedResource = temp;
 
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            } finally {
-                ms.release();
-            }
-        }
-    }
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				ms.release();
+			}
+		}
+	}
 }
 
 class MySemaphore {
-    private int count;
+	private int n; //semaforos skaitiklis
 
-    public MySemaphore(int count) {
-        this.count = count;
-    }
+	public MySemaphore(int n) {
+		this.n = n;
+	}
 
-    public int numberAvailable() {
-        return count;
-    }
+	public int numberAvailable() {
+		return n;
+	}
 
-    public synchronized void request() throws InterruptedException {
-        while (count <= 0) {
-            this.wait();
-        }
-        --count;
-    }
+	public synchronized void request() throws InterruptedException {
+		while (n <= 0) { //kol nėra available, laukiame
+			this.wait();
+		}
+		--n;
+	}
 
-    public synchronized void release() {
-        ++count;
-        if (count > 0) {
-            this.notify();
-        }
-    }
+	public synchronized void release() {
+		++n;
+		if (n > 0) { //jei yra available, pranešame visiems laukiantiems
+			this.notifyAll();
+		}
+	}
 }
